7e6529ddcaa1682734bae2f1de9ed0badd55ce97
Rodrigo Cornidez <Cornidez04@gmail.com>
2022-07-30 03:46:26 +0000

    Cleaned up directories.

--- reducers/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import themeSlice from './themeReducer.js';
-
-
-export default configureStore({
-	  reducer: {
-		theme: themeSlice.reducer,
-	  },
-})
